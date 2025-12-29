88d4aa0b79d39e9c56793f59f0bc0e422ae84807
za1dGodspeed <yunaikbar@gmail.com>
2025-12-29 18:08:23 +0700

    fresh frontend with astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
-import react from '@astrojs/react';
+
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://smsg-uinsa.vercel.app',
     vite: {
         plugins: [tailwindcss()]
     },
-    integrations: [react()],
-    adapter: netlify({
-        devFeatures: {
-            environmentVariables: true
-        }
-    })
+    adapter: netlify()
+
+
+
+
+
 });
